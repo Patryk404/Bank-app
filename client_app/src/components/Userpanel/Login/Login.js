551924e14c9d25ba55424ad44aac85dee0216b41
Patryk <patryk744@op.pl>
2020-06-13 21:44:38 +0200

    Working login

---
+++ client_app/src/components/Userpanel/Login/Login.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import classes from './Login.module.css';
+import Button from '../../UI/Button/Button';
+
+const login = (props)=>{//onSubmit={handleSubmit} 
+        return (
+            <div className={classes.Login}>
+                <label>
+                        Mail:
+                        <input type="text" name="email" onChange={props.change}/>
+                </label>
+                <label>
+                        Login:
+                        <input type="text" name="login" onChange={props.change}/>
+                </label>
+                <label>
+                        Password:
+                        <input type="text" name="password" onChange={props.change}/>
+                </label>
+                <Button click={props.submit}>Login</Button>
+                <br/>
+                {props.error}
+                {props.message}
+          </div>
+        );
+};
+
+export default login;
--- client_app/src/components/Userpanel/Userpanel.js
+++ client_app/src/components/Userpanel/Userpanel.js
@@ -1,13 +1,31 @@
 import React, {Component} from 'react';
 import Button from '../UI/Button/Button';
+import Login from '../Userpanel/Login/Login';
 import Modal from '../UI/Modal/Modal';
 import Aux from '../../hoc/Auxiliary/Auxiliary';
+import axios from 'axios';
+
 class Userpanel extends Component { 
     state = {
         showlogin: false,
-        showsignup: false
-    }
+        showsignup: false,
+        user: {
+            email: '',
+            login: '',
+            password: ''
+        },
+        error: '',
+        message: ''
+    };
 
+    handleChange = (event)=>{
+        this.setState({
+            user:{
+                ...this.state.user,
+                [event.target.name]: event.target.value
+            }
+        });
+    }
     loginButtonHandler =()=>{
         this.state.showlogin ? this.setState({showlogin: false}) :
         this.setState({showlogin: true});
@@ -16,14 +34,47 @@ class Userpanel extends Component {
         this.state.showsignup ? this.setState({showsignup: false}) :
         this.setState({showsignup: true});
     };
+    submitLoginHandler = ()=>{
+        console.log(this.state.user);
+        axios.post('http://localhost:3000/login',this.state.user)
+        .then(response=>{
+            this.setState({
+                message: response.data.message
+            });
+        })
+        .catch(err=>{
+            this.setState({
+                error: err
+            });
+        })
+    }
     render(){
+        let userPanel;
+        if (this.state.error==='')
+        {
+        userPanel = (<Aux>
+            <Button click={this.loginButtonHandler}>Log in</Button>
+            <Modal show={this.state.showlogin} clickonbackdrop={this.loginButtonHandler}>
+                <Login submit={this.submitLoginHandler} change={this.handleChange} message={this.state.message}/>
+            </Modal>
+            <Button click={this.signupButtonHandler}>Register</Button>
+            <Modal show={this.state.showsignup} clickonbackdrop={this.signupButtonHandler}>
+            </Modal>
+        </Aux>);
+        }
+        else {
+        userPanel = (<Aux>
+        <Button click={this.loginButtonHandler}>Log in</Button>
+        <Modal show={this.state.showlogin} clickonbackdrop={this.loginButtonHandler}>
+            <Login submit={this.submitLoginHandler} change={this.handleChange} error={"Something went wrong"}/>
+        </Modal>
+        <Button click={this.signupButtonHandler}>Register</Button>
+        <Modal show={this.state.showsignup} clickonbackdrop={this.signupButtonHandler}>
+        </Modal>
+    </Aux>);
+        }
         return(
-            <Aux>
-                <Button click={this.loginButtonHandler}>Log in</Button>
-                <Modal show={this.state.showlogin} clickonbackdrop={this.loginButtonHandler}/>
-                <Button click={this.signupButtonHandler}>Register</Button>
-                <Modal show={this.state.showsignup} clickonbackdrop={this.signupButtonHandler}/>
-            </Aux>
+            userPanel
         );
     }
 }
