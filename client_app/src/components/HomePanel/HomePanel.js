3e4ad26b25e9c2dbb2009864313a16521f90104b
Patryk <patryk744@op.pl>
2020-06-19 16:27:18 +0200

    Refactored login error handling

--- client_app/src/components/HomePanel/HomePanel.js
+++ client_app/src/components/HomePanel/HomePanel.js
@@ -23,7 +23,7 @@ class Userpanel extends Component { // UserPanel component which is containter
             login: '',
             password: ''
         },
-        error: '',//possible error
+        error: false,//possible error
         message: '',//possible message
         logged: false,//if we logged 
     };
@@ -91,7 +91,7 @@ class Userpanel extends Component { // UserPanel component which is containter
         })
         .catch(err=>{//if we passed wrong informations we setting error
             this.setState({
-                error: err
+                error: true
             });
         })
     }
@@ -117,7 +117,7 @@ class Userpanel extends Component { // UserPanel component which is containter
                 </Layout>
             </Aux>);
         } 
-        else if (this.state.error==='' && this.state.logged===false)// This is rendering normal if we get site first time
+        else
         {
         userPanel = (<Aux>
             <Layout>
@@ -128,30 +128,13 @@ class Userpanel extends Component { // UserPanel component which is containter
           </div>
             <Button click={this.loginButtonHandler}>Log in</Button>
             <Modal show={this.state.showlogin} clickonbackdrop={this.loginButtonHandler}>
-                <Login user={this.state.user} submit={this.submitLoginHandler} change={this.handleChangeLogin} message={this.state.message}/>
+            {
+                this.state.error ? <Login user={this.state.user} submit={this.submitLoginHandler} change={this.handleChangeLogin} error={'Something went wrong sorry :('}/> : <Login user={this.state.user} submit={this.submitLoginHandler} change={this.handleChangeLogin} message={this.state.message}/>                
+            }
             </Modal>
-            <Button>Register</Button>
+            <Button click={this.signupButtonHandler}>Register</Button>
             <Modal show={this.state.showsignup} clickonbackdrop={this.signupButtonHandler}>
                 <Signup submit={this.submitSignupHandler} message={this.state.message} change={this.handleChangeSignup} user={this.state.register_user}/>
-            </Modal>
-            </Layout>
-        </Aux>);
-        }
-        else {//this render when we get error in login 
-        userPanel = (<Aux>
-            <Layout>
-                      <div style={{fontSize: "30px"}}>
-            Hi welcome in our bank!
-            <br/>
-            Come on look at your money!
-          </div>
-            <Button click={this.loginButtonHandler}>Log in</Button>
-            <Modal show={this.state.showlogin} clickonbackdrop={this.loginButtonHandler}>
-            <Login user={this.state.user} submit={this.submitLoginHandler} change={this.handleChangeLogin} error={"Something went wrong"}/>
-            </Modal>
-                <Button click={this.signupButtonHandler}>Register</Button>
-            <Modal show={this.state.showsignup} clickonbackdrop={this.signupButtonHandler}>
-                <Signup message={this.state.message} submit={this.submitSignupHandler} change={this.handleChangeSignup} user={this.state.register_user}/>
             </Modal>
             </Layout>
         </Aux>);
@@ -164,4 +147,4 @@ class Userpanel extends Component { // UserPanel component which is containter
 
 export default Userpanel;
 
-//This code required refactoring we might create new container component if we logged into system. Not setting all this information in one container component
+//This code required refactoring we might create new container component if we logged into system. Not
