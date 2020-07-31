27b290a60d005e023d4e308a559e7c8c5094c183
Patryk <patryk744@op.pl>
2020-07-31 21:33:37 +0200

    initial commit production2

---
+++ build/precache-manifest.87d010b90d477319dd4c4f9dec280034.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53bc21c3dc15cbaf08ad45edba3941b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d47d9251e85053c13cfc",
+    "url": "/static/css/main.950e6073.chunk.css"
+  },
+  {
+    "revision": "c304067bf0b156ead5f7",
+    "url": "/static/js/2.f7cb2444.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.f7cb2444.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d47d9251e85053c13cfc",
+    "url": "/static/js/main.d2b5cf8c.chunk.js"
+  },
+  {
+    "revision": "538b47d8ecc3d2677975",
+    "url": "/static/js/runtime-main.0fe2cc16.js"
+  }
+]);
